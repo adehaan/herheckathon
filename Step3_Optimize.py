afb7e37c5e3d7828991c2a02f5e1967461034592
bjpsantos <70652819+bjpsantos@users.noreply.github.com>
2021-06-26 17:53:45 +0100

    fixed errors and created json file

--- Step3_Optimize.py
+++ Step3_Optimize.py
@@ -1,19 +1,33 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import json
 
 from sklearn.naive_bayes import GaussianNB
 import collections
 from sklearn.metrics import recall_score, f1_score, precision_score, accuracy_score
-from sklearn import preprocessing
+from sklearn import preprocessing, svm
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 def upload():
     train_data = pd.read_csv('data/train.csv')
     test_data = pd.read_csv('data/test.csv')
     return train_data, test_data
 
+def split_data(data):
+    data_wlabel = data.loc[:,data.columns != 'default']
+    label = data['default']
+    return data_wlabel, label
+
+
+def plot_histograms(data1, feature, feat_label, info):
+    sns.displot(data1, x=feature, hue=feat_label)
+    plt.title(info+' normalization')
+    plt.show()
 
 # Correlation Calculation
 def correlation(data, feature):
@@ -33,39 +47,50 @@ def correlation(data, feature):
     aux_list.remove(feature)
     return aux_list
 
-# Normalization when correlation above certain threshold
-def custom_normalization(data, feature):
-    correlated_features = correlation(data, feature)
+
+
+# Custom normalization when correlation above certain threshold
+def custom_normalization(data_train, data_test, feature):
+    # Correlation
+    correlated_features = correlation(data_train, feature)
     # subset data - only normalize high correlation
-    female_subset = data.loc[data[feature] == 1]
-    male_subset = data.loc[data[feature] == 0]
-    female_subset_normalized = female_subset.copy()
-    male_subset_normalized = male_subset.copy()
-    for i in range(len(data.columns)):
-        if data.columns[i] in correlated_features and data[data.columns[i]].dtype != 'int64' and data[
-            data.columns[i]].dtype != 'bool':
-            feat = data.columns[i]
-            plot_histograms(data, feat, feature, 'Before')
-            # female_subset_normalized[feat]= preprocessing.normalize(female_subset[feat], norm='l2')
-            female_subset_normalized[feat] = female_subset_normalized[feat] / female_subset_normalized[feat].abs().max()
-            male_subset_normalized[feat] = male_subset_normalized[feat] / male_subset_normalized[feat].abs().max()
+    female_subset = data_train.loc[data_train[feature] == 1]
+    male_subset = data_train.loc[data_train[feature] == 0]
+    female_subset_train_normalized = female_subset.copy()
+    male_subset_train_normalized = male_subset.copy()
+    data_train_normalized =data_train.copy()
+    
+    female_subset = data_test.loc[data_train[feature] == 1]
+    male_subset = data_test.loc[data_train[feature] == 0]
+    female_subset_test_normalized = female_subset.copy()
+    male_subset_test_normalized = male_subset.copy()
+    data_test_normalized =data_test.copy()
+    for i in range(len(data_train.columns)):
+        if data_train.columns[i] in correlated_features and data_train[data_train.columns[i]].dtype != 'int64' and data_train[
+            data_train.columns[i]].dtype != 'bool':
+            feat = data_train.columns[i]
+            plot_histograms(data_train, feat, feature, 'Before')
+            female_subset_train_normalized[feat] = female_subset_train_normalized[feat] / female_subset_train_normalized[feat].abs().max()
+            male_subset_train_normalized[feat] = male_subset_train_normalized[feat] / male_subset_train_normalized[feat].abs().max()
             print('custom_normalization', feat)
-            data_normalized = female_subset_normalized.append(male_subset_normalized)
-            plot_histograms(data_normalized, feat, feature, 'After')
-
-    for i in range(len(data.columns)):
-        if data.columns[i] not in correlated_features and data[data.columns[i]].dtype != 'int64' and data[
-            data.columns[i]].dtype != 'bool':
-            feat = data.columns[i]
-            data_normalized[feat] = data_normalized[feat] / data_normalized[feat].abs().max()
+            data_train_normalized = female_subset_train_normalized.append(male_subset_train_normalized)
+            plot_histograms(data_train_normalized, feat, feature, 'After')
+            
+            female_subset_test_normalized[feat] = female_subset_test_normalized[feat] / female_subset_test_normalized[feat].abs().max()
+            male_subset_test_normalized[feat] = male_subset_test_normalized[feat] / male_subset_test_normalized[feat].abs().max()
+            data_test_normalized = female_subset_test_normalized.append(male_subset_test_normalized)
+            
+    for i in range(len(data_train.columns)):
+        
+        if data_train.columns[i] not in correlated_features and data_train[data_train.columns[i]].dtype != 'int64' and data_train[data_train.columns[i]].dtype != 'bool':
+            feat = data_train.columns[i]
+            data_train_normalized[feat] = data_train_normalized[feat] / data_train_normalized[feat].abs().max()
             print('normalize', feat)
-    # print('max',max(female_subset_normalized['education']))
-    return data_normalized
 
-def split_data(data):
-    data_wlabel = data.loc[:,data.columns != 'default']
-    label = data['default']
-    return data_wlabel, label
+            data_test_normalized[feat] = data_test_normalized[feat] / data_test_normalized[feat].abs().max()
+    return data_train_normalized, data_test_normalized
+
+
 
 
 
@@ -100,18 +125,29 @@ def rf_model(train_data, test_data):
     predict = clf.predict(Y)
     return real, predict
 
-# Nearest Centroid
-def nc_model(train_data, test_data):
-    nearest_centroid = NearestCentroid()
-    nearest_centroid.fit(weighting_data_train, weighting_target_train)
-    predictions = nearest_centroid.predict(weighting_data_test)
-    print("nearest_centroid: acc: {}".format(accuracy_score(weighting_target_test, predictions)))
-
+# # Nearest Centroid
+# def nc_model(train_data, test_data):
+#     nearest_centroid = NearestCentroid()
+#     nearest_centroid.fit(weighting_data_train, weighting_target_train)
+#     predictions = nearest_centroid.predict(weighting_data_test)
+#     print("nearest_centroid: acc: {}".format(accuracy_score(weighting_target_test, predictions)))
 
 # SVN
-
-
+def svm_model(train_data, test_data):
+    X, label = split_data(train_data)
+    Y, real = split_data(test_data)
+    clf = svm.SVC()
+    clf.fit(X, label)
+    predict = clf.predict(Y)
+    return real, predict
 #kNN
+def knn_model(train_data, test_data):
+    X, label = split_data(train_data)
+    Y, real = split_data(test_data)  
+    clf = KNeighborsClassifier(n_neighbors=3, weights = 'distance')
+    clf.fit(X, label)
+    predict = clf.predict(Y)
+    return real, predict
 
 
 
@@ -143,36 +179,37 @@ def run_model(model, train_data, test_data):
 
 
 # Calculating Differences in Prediction
-def check_diff_sex(test_data2, predict):
+def check_diff_sex(test_data2):
     test_data2['predict']=pd.Series(predict)
     women_f = test_data2.loc[(test_data2["default"] != test_data2["predict"]) & test_data2["sex"]==1]
     men_f = test_data2.loc[(test_data2["default"] != test_data2["predict"]) & test_data2["sex"]==0]
-    return women_f.shape[0], men_f.shape[0]
+    test_data2 = test_data2.drop(['predict'], axis=1)
+    return test_data2, women_f.shape[0], men_f.shape[0]
 
-def check_diff_minority(test_data2, predict):
+def check_diff_minority(test_data2):
     test_data2['predict']=pd.Series(predict)
     min_f = test_data2.loc[(test_data2["default"] != test_data2["predict"]) & test_data2["minority"]==1]
     notmin_f = test_data2.loc[(test_data2["default"] != test_data2["predict"]) & test_data2["minority"]==0]
-    return min_f.shape[0], notmin_f.shape[0]
+    test_data2 =test_data2.drop(['predict'], axis=1)
+    return test_data2, min_f.shape[0], notmin_f.shape[0]
 
 
 # Rerun Models on Female subset or Male subset
 # 1 Female
 def model_female(test_data2, train_data2):
     train_data_female = train_data2.loc[train_data2["sex"]==1]
-    test_data2.drop(['predict'], axis=1)
-    f_acc, f_recall, f_precision, f_f1, f_real, f_predict = run_model(model,train_data_female, test_data2)
+    test_data_female = test_data2.loc[test_data2["sex"]==1]
+    #test_data2 = test_data2.drop(['predict'], axis=1, inplace=True)
+    f_acc, f_recall, f_precision, f_f1, f_real, f_predict = run_model(model,train_data_female, test_data_female)
     return f_acc, f_recall, f_precision, f_f1, f_real, f_predict
 
 # 2 Male
 def model_male(test_data2, train_data2):
     train_data_male = train_data2.loc[train_data2["sex"]==0]
-    test_data2.drop(['predict'], axis=1)
-    m_acc, m_recall, m_precision, m_f1, m_real, m_predict = run_model(model,train_data_male, test_data2)
+    test_data_male = test_data2.loc[test_data2["sex"]==0]
+    #test_data2 = test_data2.drop(['predict'], axis=1)
+    m_acc, m_recall, m_precision, m_f1, m_real, m_predict = run_model(model,train_data_male, test_data_male)
     return m_acc, m_recall, m_precision, m_f1, m_real, m_predict
-
-
-
 #-------------------------------------------------------
 
 #PREPROCESSING
@@ -190,8 +227,8 @@ train_data2 = train_data2.drop(['Unnamed: 0'], axis=1)
 
 # STEP 3 Normalization
 feature = 'sex'
-normalized_train = custom_normalization(train_data2, feature)
-normalized_test = custom_normalization(test_data2, feature)
+normalized_train, normalized_test= custom_normalization(train_data2, test_data2, feature)
+
 
 # STEP 4: Train Model
 # AT THE MOMENT: hardcode which model to use
@@ -201,18 +238,20 @@ model = 'randomf'
 # STEP 5 Run and Evaluate Model
 acc, recall, precision, f1, real, predict = run_model(model, normalized_train, normalized_test)
 
+
 # STEP 6 Check for Biases
-women_f, men_f = check_diff_sex(normalized_test, predict)
-min_f, notmin_f = check_diff_minority(normalized_test, predict)
+normalized_test, women_f, men_f = check_diff_sex(normalized_test)
+normalized_test, min_f, notmin_f = check_diff_minority(normalized_test)
 
 # STEP 7 Rerun Models on female and male only
 m_acc, m_recall, m_precision, m_f1, m_real, m_predict = model_male(normalized_test, normalized_train)
 f_acc, f_recall, f_precision, f_f1, f_real, f_predict = model_female(normalized_test, normalized_train)
 
-# TODO: CREATE JSON FILE FOR THESE NUMBERS
+# CREATE JSON FILE FOR THESE NUMBERS
+info = {'General_model': {'name': 'All', 'acc':acc, 'recall': recall, 'precision': precision, 'f1': f1}, 
+        'Male_model': {'name':'male', 'm_acc': m_acc, 'm_recall': m_recall, 'm_precision': m_precision, 'm_f1':m_f1},
+        'Female_model': {'name':'Female', 'f_acc': f_acc, 'f_recall': f_recall, 'f_precision': f_precision,'f_f1':f_f1},
+        'women_f': women_f, 'men_f': men_f, 'min_f': min_f, 'notmin_f':notmin_f}
 
-# In Json:
-# m_acc, m_recall, m_precision, m_f1, m_real, m_predict
-# f_acc, f_recall, f_precision, f_f1, f_real, f_predict
-# acc, recall, precision, f1, real, predict
-# women_f, men_f & min_f, notmin_f
+with open('info.json', 'w') as outfile:
+    json.dump(info, outfile)
