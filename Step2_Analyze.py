d42f11daa50f80edb4549251a7cf8a942e8c38f3
bjpsantos <70652819+bjpsantos@users.noreply.github.com>
2021-06-26 13:11:13 +0100

    fixed the errors and created the json file

--- Step2_Analyze.py
+++ Step2_Analyze.py
@@ -3,6 +3,7 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.naive_bayes import GaussianNB
+import json
 
 import collections
 from sklearn.metrics import recall_score, f1_score, precision_score, accuracy_score
@@ -23,6 +24,7 @@ def normalization(data):
         if data[data.columns[i]].dtype !='int64' and data[data.columns[i]].dtype !='bool':
             feat = data.columns[i]
             data_normalized[feat] = data_normalized[feat]/data_normalized[feat].abs().max()
+    return data_normalized
 
 
 #MODELS
@@ -62,12 +64,12 @@ def rf_model(train_data, test_data):
     predict = clf.predict(Y)
     return real, predict
 
-# Nearest Centroid
-def nc_model(train_data, test_data):
-    nearest_centroid = NearestCentroid()
-    nearest_centroid.fit(weighting_data_train, weighting_target_train)
-    predictions = nearest_centroid.predict(weighting_data_test)
-    print("nearest_centroid: acc: {}".format(accuracy_score(weighting_target_test, predictions)))
+# # Nearest Centroid
+# def nc_model(train_data, test_data):
+#     nearest_centroid = NearestCentroid()
+#     nearest_centroid.fit(weighting_data_train, weighting_target_train)
+#     predictions = nearest_centroid.predict(weighting_data_test)
+#     print("nearest_centroid: acc: {}".format(accuracy_score(weighting_target_test, predictions)))
 
 
 # SVN
@@ -105,32 +107,36 @@ def run_model(model, train_data, test_data):
 
 
 # Calculating Differences in Prediction
-def check_diff_sex():
+def check_diff_sex(test_data2):
     test_data2['predict']=pd.Series(predict)
     women_f = test_data2.loc[(test_data2["default"] != test_data2["predict"]) & test_data2["sex"]==1]
     men_f = test_data2.loc[(test_data2["default"] != test_data2["predict"]) & test_data2["sex"]==0]
-    return women_f.shape[0], men_f.shape[0]
+    test_data2 = test_data2.drop(['predict'], axis=1)
+    return test_data2, women_f.shape[0], men_f.shape[0]
 
-def check_diff_minority():
+def check_diff_minority(test_data2):
     test_data2['predict']=pd.Series(predict)
     min_f = test_data2.loc[(test_data2["default"] != test_data2["predict"]) & test_data2["minority"]==1]
     notmin_f = test_data2.loc[(test_data2["default"] != test_data2["predict"]) & test_data2["minority"]==0]
-    return min_f.shape[0], notmin_f.shape[0]
+    test_data2 =test_data2.drop(['predict'], axis=1)
+    return test_data2, min_f.shape[0], notmin_f.shape[0]
 
 
 # Rerun Models on Female subset or Male subset
 # 1 Female
-def model_female(test_data2):
+def model_female(test_data2, train_data2):
     train_data_female = train_data2.loc[train_data2["sex"]==1]
-    test_data2.drop(['predict'], axis=1)
-    f_acc, f_recall, f_precision, f_f1, f_real, f_predict = run_model(model,train_data_female, test_data2)
+    test_data_female = test_data2.loc[test_data2["sex"]==1]
+    #test_data2 = test_data2.drop(['predict'], axis=1, inplace=True)
+    f_acc, f_recall, f_precision, f_f1, f_real, f_predict = run_model(model,train_data_female, test_data_female)
     return f_acc, f_recall, f_precision, f_f1, f_real, f_predict
 
 # 2 Male
-def model_male(test_data2):
+def model_male(test_data2, train_data2):
     train_data_male = train_data2.loc[train_data2["sex"]==0]
-    test_data2.drop(['predict'], axis=1)
-    m_acc, m_recall, m_precision, m_f1, m_real, m_predict = run_model(model,train_data_male, test_data2)
+    test_data_male = test_data2.loc[test_data2["sex"]==0]
+    #test_data2 = test_data2.drop(['predict'], axis=1)
+    m_acc, m_recall, m_precision, m_f1, m_real, m_predict = run_model(model,train_data_male, test_data_male)
     return m_acc, m_recall, m_precision, m_f1, m_real, m_predict
 
 
@@ -163,19 +169,19 @@ model = 'randomf'
 acc, recall, precision, f1, real, predict = run_model(model, train_data2, test_data2)
 
 # STEP 6 Check for Biases
-women_f, men_f = check_diff_sex()
-min_f, notmin_f = check_diff_minority()
+test_data2, women_f, men_f = check_diff_sex(test_data2)
+test_data2, min_f, notmin_f = check_diff_minority(test_data2)
 
 # STEP 7 Rerun Models on female and male only
-m_acc, m_recall, m_precision, m_f1, m_real, m_predict = model_male(test_data2)
-f_acc, f_recall, f_precision, f_f1, f_real, f_predict = model_female(test_data2)
+m_acc, m_recall, m_precision, m_f1, m_real, m_predict = model_male(test_data2, train_data2)
+f_acc, f_recall, f_precision, f_f1, f_real, f_predict = model_female(test_data2, train_data2)
 
 # TODO: CREATE JSON FILE FOR THESE NUMBERS
 
-info = {'General_model': [{'name':'All'}, {'values':[acc, recall, precision, f1, real, predict]}], 
-        'Male_model': [{'name':'male'}, {'values':[m_acc, m_recall, m_precision, m_f1, m_real, m_predict]}],
-        'Female_model': [{'name':'Female'}, {'values':[f_acc, f_recall, f_precision, f_f1, f_real, f_predict]}],
-        'women_f':women_f, 'men_f': men_f, 'min_f': min_f, 'notmin_f':notmin_f}
+info = {'General_model': {'name': 'All', 'acc':acc, 'recall': recall, 'precision': precision, 'f1': f1}, 
+        'Male_model': {'name':'male', 'm_acc': m_acc, 'm_recall': m_recall, 'm_precision': m_precision, 'm_f1':m_f1},
+        'Female_model': {'name':'Female', 'f_acc': f_acc, 'f_recall': f_recall, 'f_precision': f_precision,'f_f1':f_f1},
+        'women_f': women_f, 'men_f': men_f, 'min_f': min_f, 'notmin_f':notmin_f}
 
 with open('info.json', 'w') as outfile:
     json.dump(info, outfile)
